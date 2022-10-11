48241cebe0918a493474a2c208888c26cbb2c6f4
SaadiaElf <saadiaelfekak@gmail.com>
2022-10-11 17:19:20 +0100

    Fixed logic bug

--- mandatory/debugging-practice/script.js
+++ mandatory/debugging-practice/script.js
@@ -78,7 +78,7 @@ function render() {
     changeBut.className = "btn btn-success";
     cell4.appendChild(changeBut);
     let readStatus = "";
-    if (myLibrary[i].check == false) {
+    if (myLibrary[i].check == true) {
       readStatus = "Yes";
     } else {
       readStatus = "No";
